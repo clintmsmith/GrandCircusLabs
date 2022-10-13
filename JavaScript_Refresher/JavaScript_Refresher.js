f44d28c9bd5ce2a85d6e775190cfd80f5ee1aab9
Clint Smith <clintsmith@rocketmortgage.com>
2022-10-13 15:49:58 -0700

    combining labs

---
+++ JavaScript_Refresher/JavaScript_Refresher.js
@@ -0,0 +1,21 @@
+let firstName = "Clint";
+let lastName ="Smith";
+let fullName = firstName+" "+lastName;
+console.log (fullName);
+
+
+let population = 4584000;
+//Population of Phoenix (2021)
+console.log (population);
+console.log (population * 3);
+
+let javaScriptIsCool = true;
+let colors = ["red, orange, yellow, green, blue, violet"];
+
+for (let i = 1; i <= 5; i++) {
+    console.log (i);
+}
+
+for (let i = 0; i <= 4; i++) {
+    console.log (i);
+}
